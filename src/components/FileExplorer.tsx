720caa436c29053267f04b91c3753f134e5b5c97
SpiderKingWynn <skwofficial@outlook.com>
2024-02-09 14:24:38 +0800

    Update Version 0.1.0 (Terminal, Control, FileExplorer, macOS Arm, Windows x86, Windows arm64)

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -1,14 +1,11 @@
 import "./FileExplorer.css"
 import { dialog, fs } from "@tauri-apps/api"
 
-async function OpenProject(propsop: any) {
-    //document.getElementById("openproject")?.classList.add("buttonloading")
+async function OpenProject() {
     const selected = await dialog.open({
         directory: true,
         multiple: false,
     });
-
-    propsop = selected;
 
     if (selected != null){
         const entries = await fs.readDir(selected as string);
@@ -39,7 +36,7 @@ const FileExplorer = (props: any) => {
                 <div>
                     <h4>You have not yet opened a project.</h4>
                     <button>New Project</button><br />
-                    <button onClick={() => OpenProject(props.dir)} id="openproject">Open Project</button>
+                    <button onClick={() => OpenProject()} id="openproject">Open Project</button>
                 </div>
             </div>
         )
